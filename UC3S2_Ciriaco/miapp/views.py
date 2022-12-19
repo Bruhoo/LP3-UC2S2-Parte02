da360a64b08d4311834be359410f199c648aeb2e
Omar Ciriaco <2014200877@untels.edu.pe>
2022-12-19 11:14:47 -0500

    UC3S2_Parte02_Ciriaco

--- UC3S2_Ciriaco/miapp/views.py
+++ UC3S2_Ciriaco/miapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+layout="""
+    <h1>Lenguaje de Programacion III-S2-EC3</h1>
+    <hr/>
+    <ul>
+        <li>
+            <a href="/inicio">Inicio</a>
+        </li>
+    </ul>
+    <hr/>
+"""
+def index(request):
+    mensaje="""
+        <h3>Ciriaco Esquivel Omar Antonio</h2>
+        <h3>Cachay Huertas Jose Fernando</h3>
+        <h3>Delgado Tuanama Willy Francis</h3>
+    """
+    return HttpResponse(layout + mensaje)
